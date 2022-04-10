01dcb972399d4a99394cba5226fddc35f0440789
HoldenLalumiere <holdenlalumiere@gmail.com>
2022-04-10 02:53:48 -0400

    working on getting a dropdown menu to work

--- react-app/src/components/navigationBar.jsx
+++ react-app/src/components/navigationBar.jsx
@@ -1,12 +1,31 @@
 import React, {Component} from "react";
+import navigationBar from "./navigationBar.css"
 
 export default class NavigationBar extends Component{
+    dropdown() {
+        document.getElementById("dropdown").classList.toggle("show");
+    }
+
     render() {
         return (
-            <div>
+            <div class="navBar">
                 <a href="home">Home</a>
-                <a href="news">News</a>
+                <div className="searchBar">
+                    <button onClick="dropdown()">placeholder</button>
+                    <div className="dropdownContent" id="dropdown">
+                        <a>Alg1</a>
+                        <a>Alg2</a>
+                        <a>Alg3</a>
+                    </div>
+
+
+                    <input className="search" type="text" placeholder="Search..."/>
+                    <button type="submit">Submit</button>
+                </div>
             </div>
         );
     }
+
+
+
 }
