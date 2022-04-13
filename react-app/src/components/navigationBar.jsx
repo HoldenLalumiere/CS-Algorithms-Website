dac707d375aa948dd4b19d03db0a9b468041ef2c
HoldenLalumiere <holdenlalumiere@gmail.com>
2022-04-13 00:05:27 -0400

    Got a working drop down menu

--- react-app/src/components/navigationBar.jsx
+++ react-app/src/components/navigationBar.jsx
@@ -2,27 +2,31 @@ import React, {Component} from "react";
 import navigationBar from "./navigationBar.css"
 
 export default class NavigationBar extends Component{
-    dropdown() {
+
+    handleDropdown() {
+        document.getElementById("dropdown").classList.toggle("dropdownContent");
         document.getElementById("dropdown").classList.toggle("show");
+        console.log(document.getElementById("dropdown").classList)
     }
 
     render() {
         return (
-            <div class="navBar">
-                <a href="home">Home</a>
-                <div className="searchBar">
-                    <button onClick="dropdown()">placeholder</button>
-                    <div className="dropdownContent" id="dropdown">
-                        <a>Alg1</a>
-                        <a>Alg2</a>
-                        <a>Alg3</a>
+            <React.Fragment>
+                <div className="navBar">
+                    <a href="home">Home</a>
+                    <div className="searchBar">
+                        <button onClick={() => this.handleDropdown()}>placeholder</button>
+
+                        <input className="search" type="text" placeholder="Search..."/>
+                        <button type="submit">Submit</button>
+                        <div className="dropdownContent" id="dropdown">
+                            <a>Alg1</a>
+                            <a>Alg2</a>
+                            <a>Alg3</a>
+                        </div>
                     </div>
-
-
-                    <input className="search" type="text" placeholder="Search..."/>
-                    <button type="submit">Submit</button>
                 </div>
-            </div>
+            </React.Fragment>
         );
     }
 
